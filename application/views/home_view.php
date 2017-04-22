528f77f22c134416550988d726f85ecff8610db5
chanjr152 <tobeapatis@gmail.com>
2017-04-23 04:52:15 +0700

    Add files via upload

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -17,12 +17,91 @@
  </div>
 <script src="<?php echo base_url("assets/js/bootstrap.js"); ?>" ></script>
 
-<div class="filter1">
+
+<div class="filter1" >
 		<ul class="container-filter1">
-			<li class="active"><a href="index.html">Display <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true" ></span></a></li>
-			<li class="grid"><a href="#">Category <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true" ></span></a></li>
-			<li class="grid"><a href="#">Style <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true" ></span></a></li>
-			<li class="grid"><a href="typo.html">Price <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true" ></span></a></li>					
+		<!-- <ul class="memenu skyblue"> -->
+			<li class="grid">
+				<a href="#">Display <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true" ></span></a>
+				<div class="mepanel">
+							<div class="row">
+								<div class="col1 me-one">
+									<h4>View</h4>
+									<ul>
+										<li><a href="product.html" ><span class="glyphicon glyphicon-user"></span> Designer</a></li>
+										<li><a href="product.html"><span class="glyphicon glyphicon-user"></span> Artist</a></li>
+										<li><a href="product.html"><span class="glyphicon glyphicon-user"></span> Brands</a></li>
+									</ul>
+								</div>
+							</div>
+				</div>
+			</li>
+			<li class="grid"><a href="#">Category <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true" ></span></a>
+				<div class="mepanel">
+					<div class="row">
+						<div class="col1 me-one">
+							<h4>Shop</h4>
+							<ul>
+								<li><a href="product.html"><span class="glyphicon glyphicon-user"></span> Furniture</a></li>
+								<li><a href="product.html"><span class="glyphicon glyphicon-user"></span> Accecories</a></li>
+								<li><a href="product.html"><span class="glyphicon glyphicon-user"></span> Artwork/Decorative</a></li>
+							</ul>
+					</div>
+				</div>
+			</li>
+			<li class="grid"><a href="#">Style <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true" ></span></a>
+				<div class="mepanel">
+					<div class="row">
+						<div class="col1 me-one">
+							<h4>Shop</h4>
+							<ul>
+								<li><a href="product.html"><span class="glyphicon glyphicon-user"></span> Furniture</a></li>
+								<li><a href="product.html"><span class="glyphicon glyphicon-user"></span> Accecories</a></li>
+								<li><a href="product.html"><span class="glyphicon glyphicon-user"></span> Artwork/Decorative</a></li>
+								<table>
+										<thead>
+											<tr>
+											    <th>FURNITURE</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>
+													<ul>
+														<li><a href="#">Chair</a></li>
+														<li><a href="#">Sofa</a></li>
+														<li><a href="#">Bench</a></li>
+														<li><a href="#">Table</a></li>
+													</ul>
+												</td>
+											</tr>
+										</tbody>
+								</table>
+								<table>
+										<thead>
+											<tr>
+											    <th>FURNITURE</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td>
+													<ul>
+														<li><a href="#">Easy Chair</a></li>
+														<li><a href="#">Arm Chair</a></li>
+														<li><a href="#">Wings Chair</a></li>
+													</ul>
+												</td>
+											</tr>
+										</tbody>
+								</table>
+							</ul>
+
+					</div>
+				</div>
+			</li>
+			<li class="grid"><a href="typo.html">Price <span class="glyphicon glyphicon-triangle-bottom" aria-hidden="true" ></span></a>
+			</li>					
 		</ul>
 </div>
 <div class="clearfix"></div>
