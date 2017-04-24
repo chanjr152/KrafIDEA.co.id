ba144e386b6daa1eec3e5cf1817b12313f9ecad0
chanjr152 <tobeapatis@gmail.com>
2017-04-24 16:25:55 +0700

    commit tambah kelas supplier

---
+++ application/views/supplier_view.php
@@ -0,0 +1,77 @@
+ <div class="clearfix"></div>
+<div class="supplier">
+	<div class="supplier-profil">
+		<div class="supplier-logo">
+			<img src="<?php echo base_url().'assets/images/slider/bnr.jpg'; ?>" width="100" height="200" class="img-circle">
+			<div class="clearfix"></div>
+			<label> WH (Wrong for Hay) </label>
+			<h4>Designer Product</h4>
+		</div>
+		<div class="clearfix"></div>
+		<div class="supplier-record">
+			<div class="supplier-record-container">
+				<label><i class="fa fa-map fa-lg" aria-hidden="true"></i> Bandung, Jawa Barat <br/> Jl. Gatot Subroto No.68</label>
+			
+				<label><i class="fa fa-eye fa-lg" aria-hidden="true"></i> Display Views</label>
+				<label style="float:  right;">240</label>
+			
+				<label><i class="fa fa-thumbs-o-up fa-lg" aria-hidden="true"></i> Appreciations</label>
+				<label style="float:  right;">240</label>
+			
+				<label><i class="fa fa-sign-language fa-lg" aria-hidden="true"></i> Transactions</label>
+				<label style="float:  right;">240</label>
+			</div>
+		</div>
+		<div class="supplier-contact">
+			<div class="supplier-contact-container">
+				<label>W : www.wh.com </label>
+				<label>E : WH@info.com </label>
+				<label>P : +62852669005 </label>
+				<label>S : <i class="fa fa-instagram fa-lg" aria-hidden="true"> &nbsp;</i> <i class="fa fa-facebook fa-lg" aria-hidden="true"> </i> </label>
+			</div>
+		</div>
+		<div class="share">
+			<div class="share-container">
+				<label>Share</label>
+				<label><i class="fa fa-instagram fa-lg"></i>&nbsp;<i class="fa fa-facebook-square fa-lg"></i>&nbsp;<i class="fa fa-twitter-square fa-lg"></i> </label>
+			</div>
+		</div>
+	</div>
+	<div class="supplier-produk">
+		<div class="supplier-produk-container">
+			<div class="produk">
+				<img src="../assets/images/product/img3.jpg">
+			</div>
+			<div class="produk">
+				<img src="../assets/images/product/img3.jpg">
+			</div>
+			<div class="produk">
+				<img src="../assets/images/product/img3.jpg">
+			</div>
+			<div class="produk">
+				<img src="../assets/images/product/img3.jpg">
+			</div>
+		</div>
+		<div class="supplier-produk-container">
+			<div class="produk">
+				<img src="../assets/images/product/img3.jpg">
+			</div>
+			<div class="produk">
+				<img src="../assets/images/product/img3.jpg">
+			</div>
+			<div class="produk">
+				<img src="../assets/images/product/img3.jpg">
+			</div>
+			<div class="produk">
+				<img src="../assets/images/product/img3.jpg">
+			</div>
+		</div>
+		<div class="supplier-produk-container">
+			
+		</div>
+		<div class="supplier-produk-container">
+			
+		</div>
+	</div>
+</div>
+<div class="clearfix"></div>
