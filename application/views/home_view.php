33035caf63592e4b8ec1c89b467eedcca7fa26fe
Your Name <wiganda94@gmail.com>
2017-05-15 11:31:55 +0700

    Improve navbar mobile

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/added/page/homeMobile.css"); ?>">
 <script type="text/javascript" src="<?php echo base_url("assets/added/page/homeMobile.js"); ?>" ></script>
 
-<div class="slideshow-container">
+<!-- <div class="slideshow-container">
 	<?php echo $slide ?>
 	<a class="prev" onclick="plusSlides(this, -1)">&#10094;</a>
 	<a class="next" onclick="plusSlides(this, 1)">&#10095;</a>
@@ -10,6 +10,53 @@
 	<span class="dot" onclick="currentSlide(1)"></span> 
 	<span class="dot" onclick="currentSlide(2)"></span> 
 	<span class="dot" onclick="currentSlide(3)"></span> 
+</div> -->
+
+<div class="container">
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+	</ol>
+
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner">
+		<!-- <?php echo $slide ?> -->
+		<div class="item active">
+				<img src="<?=base_url('assets/images/slider/bnr.jpg');?>" alt="Los Angeles">
+				<div class="carousel-caption">
+					<h3>Title 1</h3>
+					<p>Deskripsi caption 1</p>
+				</div>
+			</div>
+			<div class="item">
+				<img src="<?=base_url('assets/images/slider/bnr2.jpg');?>" alt="Chicago">
+				<div class="carousel-caption">
+					<h3>Title 1</h3>
+					<p>Deskripsi caption 1</p>
+				</div>
+			</div>
+			<div class="item">
+				<img src="<?=base_url('assets/images/slider/bnr3.jpg');?>" alt="New York">
+				<div class="carousel-caption">
+					<h3>Title 1</h3>
+					<p>Deskripsi caption 1</p>
+				</div>
+			</div>
+	</div>
+
+	<!-- Left and right controls -->
+	<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="right carousel-control" href="#myCarousel" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
 </div>
 
 <div class="filter1" >
@@ -112,3 +159,5 @@
 	 	</div>
 	</div> 
 </div>
+
+<a id="back-to-top" href="#" class="btn cst-btnColor btn-md back-to-top" role="button" title="Click to return on the top page" data-toggle="tooltip" data-placement="left"><i class="fa fa-arrow-up"></i></a>
--- application/views/komponen/topnavbarMobile.php
+++ application/views/komponen/topnavbarMobile.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-green visible-xs">
+<nav class="navbar navbar-green visible-xs navbar-fixed-top">
   
     <div class="navbar-header">
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -7,6 +7,25 @@
         <span class="icon-bar"></span>                        
       </button>
       <a class="navbar-brand" href="<?=base_url();?>">krafIDEA.co.id</a>
+      <button type="button" class="navbar-toggle cst-btnnavbar" data-toggle="collapse" data-target="#mySearch">
+        <span class="fa fa-search"></span>
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="mySearch">
+      <ul class="nav navbar-nav ">
+        <li>
+          <form action="javascript:void(0);" onsubmit="alert('Sorry, under construction'); console.log('Sorry, under construction');">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search">
+        <div class="input-group-btn">
+          <button class="btn btn-default" type="submit">
+            <i class="glyphicon glyphicon-search"></i>
+          </button>
+        </div>
+      </div>
+    </form>
+        </li>
+      </ul>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
